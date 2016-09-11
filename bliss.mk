e0ef3864f371cbe1c378e539a2ae3cd07990b91d
electrikjesus <electrikjesus@gmail.com>
2016-09-11 15:11:41 -0400

    Update

--- bliss.mk
+++ bliss.mk
@@ -21,8 +21,8 @@
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
-$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/common.mk)
+# $(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := bliss_bullhead
 PRODUCT_DEVICE := bullhead
